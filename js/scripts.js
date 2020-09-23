7ee750d41b3ae15ea2af8109d8f72b289be1c6db
Kody Morse <kodylmorse@users.noreply.github.com>
2020-09-23 11:17:57 -0400

    css navbar tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-// When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
+// // When the user scrolls down 50px from the top of the document, resize the header's font size
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 50) {
-    document.getElementById("mainlogo").style.width = "54px";
-  } else {
-    document.getElementById("mainlogo").style.width = "54px";
-  }
-} 
+// function scrollFunction() {
+//   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 50) {
+//     document.getElementsByID("mainlogo").style.display = "none";
+//   } else {
+//     document.getElementsByID("mainlogo").style.width = "54px";
+//   }
+// } 
